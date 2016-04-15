f7221a1454dcc00b972f0a54efb252d629069076
Stung <stephen.tung@candrholdings.com>
2016-04-16 00:53:02 +0800

    Added very simple echo bot

---
+++ gwab-bot/Controllers/MessagesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Utilities;
+using Newtonsoft.Json;
+
+namespace gwab_bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                // calculate something for us to return
+                int length = (message.Text ?? string.Empty).Length;
+
+                // return our reply to the user
+                return message.CreateReplyMessage($"You sent {length} characters");
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ gwab-bot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gwab_bot.WebApiApplication" Language="C#" %>
